564ff30907b8d8403ba73cf2ab7e8ed6f978ebec
akshaikrish <akshaikrishna@gmail.com>
2019-07-15 14:05:56 +0530

    Test files added

--- src/app.js
+++ src/app.js
@@ -5,6 +5,33 @@ import './scss/main.scss';
 import './assets/scripts/demo';
 
 $(document).ready(() => {
+
+  /* ========================================== 
+scrollTop() >= 300
+Should be equal the the height of the header
+========================================== */
+//-Sticky navigation bar
+$(window).scroll(function(){
+  if ($(window).scrollTop() >= 300) {
+      $('nav').addClass('fixed-header');
+      $('nav div').addClass('visible-title');
+  }
+  else {
+      $('nav').removeClass('fixed-header');
+      $('nav div').removeClass('visible-title');
+  }
+});
+
+//-Hero banner with parallax scrolling
+$(window).scroll(function(e){
+  parallax();
+});
+
+function parallax(){
+  var scrolled = $(window).scrollTop();
+  $('.background').css('top',-(scrolled*0.15)+'px');
+}
+
   console.log('Ready!');
 
   require('./assets/scripts/demo');
